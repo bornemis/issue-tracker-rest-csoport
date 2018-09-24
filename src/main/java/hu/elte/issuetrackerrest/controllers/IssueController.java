38609c7910cb407cd18f7d65bdc76358a80e4579
hbddn5 <hbddn5@pc9w02.inf.elte.hu>
2018-09-24 10:53:01 +0200

    Update and delete methods in issuecontroller.

--- src/main/java/hu/elte/issuetrackerrest/controllers/IssueController.java
+++ src/main/java/hu/elte/issuetrackerrest/controllers/IssueController.java
@@ -5,9 +5,11 @@ import hu.elte.issuetrackerrest.repositories.IssueRepository;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -39,4 +41,29 @@ public class IssueController {
         Issue savedIssue = issueRepository.save(issue);
         return ResponseEntity.ok(savedIssue);
     }
+    /*
+    modositas, mi alapjan modositunk, melyik vegponton leszek
+    path-ból kiolvassa az id-t, a htttp uzenetből kiolvassa az issue objectuomot,
+    beadandónál a pathVariable az egyenlő-e, mint az issue id-ja, ha nem, akk badrequest hiba
+    */
+    @PutMapping("/{id}")
+    public ResponseEntity<Issue> update(@PathVariable Integer id, @RequestBody Issue issue){
+        Optional<Issue> oIssue = issueRepository.findById(id);
+        if (oIssue.isPresent()) {
+            issue.setId(id); //igy nem kell lekezelni, hogy a pathVariable és az issue variable-je egyenlő-e
+            return ResponseEntity.ok(issueRepository.save(issue));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Issue> delete(@PathVariable Integer id){
+    Optional<Issue> oIssue = issueRepository.findById(id);
+        if (oIssue.isPresent()) {
+            issueRepository.deleteById(id);
+            return ResponseEntity.ok().build(); //ha az ok()-nak nincs paramétere, akkor build()-et kell utána írni!
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+}
 }
